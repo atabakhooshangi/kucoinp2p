d02027b2608014a78ef3910a9ad0972c58e4469d
atabak-hooshangi <doom_horn@yahoo.com>
2024-03-27 02:31:22 +0100

    data fetch using requests

--- src/main.py
+++ src/main.py
@@ -1,13 +1,38 @@
+from contextlib import asynccontextmanager
+import requests
 from fastapi import FastAPI
+from pathlib import Path
+from dotenv import load_dotenv
+from fastapi_utils.tasks import repeat_every
 
-app = FastAPI()
 
+env_path = Path(f"/.env")
+load_dotenv(dotenv_path=env_path)
+
+
+
+
+app = FastAPI(
+    title="kucoinp2p")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.on_event('startup')
+@repeat_every(seconds=200)
+async def say_hello():
+    data = requests.get("https://www.kucoin.com/_api/otc/ad/list?status=PUTUP&currency=USDT&legal=EUR&page=1&pageSize=10&side=BUY&amount=&payTypeCodes=REVOLUT&lang=en_US")
+    highest_price = data.json()['items'][0]['floatPrice']
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
